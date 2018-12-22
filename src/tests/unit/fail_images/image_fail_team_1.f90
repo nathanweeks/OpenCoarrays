a51bf38655928cfe9f49013f2cdcdc1f6e9776fe
Nathan T. Weeks <weeks@iastate.edu>
2018-12-22 15:57:50 -0600

    Updates to image_fail_team_1

--- src/tests/unit/fail_images/image_fail_team_1.f90
+++ src/tests/unit/fail_images/image_fail_team_1.f90
@@ -3,22 +3,24 @@ program image_fail_team_1
   implicit none
 
   integer :: team_num
-  type(team_type) :: team
+  type(team_type) :: odd_even_team, initial_team_minus_failed_images
    
-! FIXME: doesn't work with ASSOCIATE?
-! associate(np => num_images(), me => this_image())
-    if (mod(np, 2) == 1 .or. np < 6) error stop "I need an even number of images (>= 6) to function"
-    team_num = mod(me, 2) + 1
-    form team (team_num, team)
+  if (mod(num_images(), 2) == 1 .or. num_images() < 6) error stop "I need an even number of images (>= 6) to function"
+  team_num = mod(this_image(), 2) + 1
+  form team (team_num, odd_even_team)
  
-    change team (team)
-      if (this_image() == 1 .or. this_image() == num_images()) fail image
-    end team
+  change team (odd_even_team)
+    if (this_image() == num_images()) fail image
+  end team
 
-    !if (size(failed_images()) /= 2) error stop "failed_images()'s size should be two."
-    write(*,*) me, '/', np, 'failed_images() == ', failed_images()
+  !if (size(failed_images()) /= 2) error stop "failed_images()'s size should be two."
+  form team (1, initial_team_minus_failed_images)
   
-!   write(*,*) me, '/', np, 'Test passed'
- end associate
+  change team (initial_team_minus_failed_images)
+    write(*,*) this_image(), '/', num_images(), 'failed_images() == ', failed_images()
+  end team
+  
+  write(*,*) 'after', this_image(), '/', num_images(), 'failed_images() == ', failed_images()
+  write(*,*) this_image(), '/', num_images(), 'Test passed'
 
 end program
